ff5f20e0566d28bfee479fc90cd8d5044ae9221d
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-15 16:03:01 +0100

    Restarting the Game ✅♻

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -147,9 +147,11 @@ namespace Tetris
             await GameLoop();
         }
 
-        private void PlayAgain_Click(object sender, RoutedEventArgs e)
+        private async void PlayAgain_Click(object sender, RoutedEventArgs e)
         {
-
+            gameState = new GameState();
+            GameOverMenu.Visibility = Visibility.Hidden;
+            await GameLoop();
         }
     }
 }
