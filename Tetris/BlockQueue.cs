d9180bbb81d65c47698e9b46d9f43f932e49afd4
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-15 13:58:22 +0100

    Picking The Block 🌼✅

---
+++ Tetris/BlockQueue.cs
@@ -0,0 +1,44 @@
+﻿namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new J_Block(),
+            new L_Block(),
+            new OBlock(),
+            new S_Block(),
+            new T_Block(),
+            new Z_Block()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+
+        private Block RandomBlock() //returns a random block
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            } while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
